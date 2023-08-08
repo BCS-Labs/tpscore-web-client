0653256f577e89cd3c0200770ed3a435134f1c7d
fromaline <grechino@protonmail.com>
2023-08-08 15:30:28 +0300

    replace v8 coverage provider with istanbul

--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,11 @@ import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: {},
+  test: {
+    coverage: {
+      provider: 'istanbul',
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
